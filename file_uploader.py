34ec32f71073ba71495a3caa2446c534e27149ad
OgeonX <OgeonX@gmail.com>
2023-04-12 02:27:33 +0300

    Commited from OpenAI API

---
+++ file_uploader.py
@@ -0,0 +1,24 @@
+
+
+# file_uploader.py
+import requests
+import os
+
+def upload_file_to_openai(api_key, file_path):
+    endpoint_url = "https://api.openai.com/v1/files"
+
+    headers = {
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    with open(file_path, 'rb') as file:
+        # Prepare the file as a multipart/form-data
+        file_data = {
+            'file': (os.path.basename(file_path), file, 'application/octet-stream'),
+            'purpose': (None, 'fine-tune')
+        }
+
+        try:
+            response = requests.post(endpoint_url, headers=headers, files=file_data)
+            if response.status_code == 201:
+                print
