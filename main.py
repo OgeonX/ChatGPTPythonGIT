16d8237989213208fb06cb3f2dd01a0c4eb85771
OgeonX <OgeonX@gmail.com>
2023-04-12 04:41:26 +0300

    Commited from OpenAI API

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 
-
-import os
-import requests
-from dotenv import load_dotenv
-from testapikey import test_api_key
-from github_uploader import upload_to_github
-import logging
-
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-GITHUB_API_KEY = os.getenv("GITHUB_API_KEY")
-GITHUB_REPO_OWNER = os.getenv("GITHUB_REPO_OWNER")
-GITHUB_REPO_NAME = os.getenv("GITHUB_REPO_NAME")
-
-logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
-
-def generate_prompt_with_code(api_key
+1. Move the code for loading environment variables and for testing the API keys to a separate file, where it can be imported and re-used. 
+2. Move the code for logging errors to a separate logging module, and only import the logging module when needed. 
+3. Move the code for uploading the modified code to GitHub to a separate utility module, and only import the utility module when needed. 
+4. Move the code for generating the prompt with code to a separate function, and only call the function when needed. 
+5. Move the list comprehension for listing all .py files in the directory to a separate list of files, and only iterate over the list when needed. 
+6. Add comments to the code so that it is easier to maintain.
