822ebe0e5ed3c690203cf4c8f5790c4762c60df6
OgeonX <OgeonX@gmail.com>
2023-04-12 03:01:17 +0300

    Commited from OpenAI API

--- github_uploader.py
+++ github_uploader.py
@@ -1,4 +1,7 @@
 
+```
+import requests
+import base64
 
 def upload_to_github(github_api_key, repo_owner, repo_name, branch, path, content, commit_message):
     headers = {
@@ -16,4 +19,25 @@ def upload_to_github(github_api_key, repo_owner, repo_name, branch, path, conten
             sha = response.json()["sha"]
 
         # Update or create the file
-        content_base64 = base
+        content_base64 = base64.b64encode(content.encode("utf-8")).decode("utf-8")
+        data = {
+            "message": commit_message,
+            "content": content_base64,
+            "branch": branch,
+        }
+        if sha:
+            data["sha"] = sha
+
+        response = requests.put(api_url, headers=headers, json=data)
+
+        if response.status_code in [200, 201]:
+            print(f"File successfully uploaded to GitHub: {path}")
+            return True
+        else:
+            print(f"File upload to GitHub failed with status code {response.status_code}: {response.text}")
+            return False
+    else:
+        print(f"File retrieval from GitHub failed with status code {response.status_code}: {response.text}")
+        return False
+
+```
