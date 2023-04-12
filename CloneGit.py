e3b622b2f807b551c549f7a074f3744a44d2c52d
OgeonX <OgeonX@gmail.com>
2023-04-12 03:00:47 +0300

    Commited from OpenAI API

--- CloneGit.py
+++ CloneGit.py
@@ -1,5 +1,4 @@
 
-
 ```
 import os
 import shutil
@@ -12,9 +11,8 @@ def clone_git_repository(git_repo_url, tmp_dir):
     # Clone the GitHub repository to a temporary directory
     os.system(f"git clone --depth 1 --branch HEAD {git_repo_url} {tmp_dir}")
 
-    # Remove the .git directory from the cloned repository
-    shutil.rmtree(os.path.join(tmp_dir, ".git"))
+    # Raise an exception if the repository was not successfully cloned
+    if not os.path.exists(tmp_dir):
+        raise Exception(f"Repository {git_repo_url} was not successfully cloned.")
 
-    # Return the temporary directory path
-    return tmp_dir
 ```
