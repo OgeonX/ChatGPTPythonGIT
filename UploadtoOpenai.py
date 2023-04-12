60e6266635dfa6d7171bee1f11175ba4d3b39e2d
OgeonX <OgeonX@gmail.com>
2023-04-12 04:42:01 +0300

    Commited from OpenAI API

--- UploadtoOpenai.py
+++ UploadtoOpenai.py
@@ -1,11 +1,8 @@
 
-
-- Changed the name of the `test_api_key` function to `test_api_key` for better readability.
-- Added a docstring to the `test_api_key` function.
-- Changed the name of the `upload_to_openai` function to `upload_to_openai` for better readability.
-- Added a docstring to the `upload_to_openai` function.
-- Changed the name of the `python_dir` variable to `python_dir` for better readability.
-- Changed the name of the `file_contents` variable to `file_contents` for better readability.
-- Changed the name of the `encoding` variable to `encoding` for better readability.
-- Changed the name of the `uploaded_file_id` variable to `uploaded_file_id` for better readability.
-- Added a `try-except` block to handle `UnicodeDecodeError` exceptions.
+1. Create separate functions for making the GET and POST requests to the OpenAI API endpoint.
+2. Move the logic for loading the API key from the config file into a separate function.
+3. Extract the logic for iterating over the files in the given directory and reading their contents into a separate function.
+4. Create a separate function for verifying the uploaded files using the OpenAI's Files API.
+5. Move the test_api_key() function into a separate module and import it when needed.
+6. Create a helper function to handle any exceptions that may occur while decoding a file.
+7. Refactor the code to use the logging module instead of printing out messages.
