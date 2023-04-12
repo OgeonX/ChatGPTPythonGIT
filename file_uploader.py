e5d464edb550559cfb98fd23dd5804db970ec391
OgeonX <OgeonX@gmail.com>
2023-04-12 03:01:00 +0300

    Commited from OpenAI API

--- file_uploader.py
+++ file_uploader.py
@@ -1,5 +1,6 @@
 
 
+```
 # file_uploader.py
 import requests
 import os
@@ -11,14 +12,23 @@ def upload_file_to_openai(api_key, file_path):
         "Authorization": f"Bearer {api_key}"
     }
 
-    try:
-        with open(file_path, 'rb') as file:
-            # Prepare the file as a multipart/form-data
-            file_data = {
-                'file': (os.path.basename(file_path), file, 'application/octet-stream'),
-                'purpose': (None, 'fine-tune')
-            }
+    with open(file_path, 'rb') as file:
+        # Prepare the file as a multipart/form-data
+        file_data = {
+            'file': (os.path.basename(file_path), file, 'application/octet-stream'),
+            'purpose': (None, 'fine-tune')
+        }
 
+        try:
             response = requests.post(endpoint_url, headers=headers, files=file_data)
             if response.status_code == 201:
-                print
+                print(f"File '{file_path}' successfully uploaded.")
+                return response.json()
+            else:
+                print(f"File upload failed with status code {response.status_code}: {response.text}")
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"File upload failed with error: {e}")
+            return None
+
+```
