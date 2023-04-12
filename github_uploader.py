3f76abafa5b496d66ae17f7d8487b6e4f91959e7
OgeonX <OgeonX@gmail.com>
2023-04-12 03:35:22 +0300

    Add files via upload

---
+++ github_uploader.py
@@ -0,0 +1,39 @@
+import requests
+import base64
+
+def upload_to_github(github_api_key, repo_owner, repo_name, branch, path, content, commit_message):
+    headers = {
+        "Authorization": f"token {github_api_key}",
+        "Content-Type": "application/json",
+    }
+
+    # Get the file's current state
+    api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
+    response = requests.get(api_url, headers=headers, params={"ref": branch})
+
+    if response.status_code in [200, 404]:
+        sha = None
+        if response.status_code == 200:
+            sha = response.json()["sha"]
+
+        # Update or create the file
+        content_base64 = base64.b64encode(content.encode("utf-8")).decode("utf-8")
+        data = {
+            "message": commit_message,
+            "content": content_base64,
+            "branch": branch,
+        }
+        if sha:
+            data["sha"] = sha
+
+        response = requests.put(api_url, headers=headers, json=data)
+
+        if response.status_code in [200, 201]:
+            print(f"File successfully uploaded to GitHub: {path}")
+            return True
+        else:
+            print(f"File upload to GitHub failed with status code {response.status_code}: {response.text}")
+            return False
+    else:
+        print(f"File retrieval from GitHub failed with status code {response.status_code}: {response.text}")
+        return False
---
+++ testapikey.py
@@ -0,0 +1,28 @@
+import requests
+
+def test_api_key(api_key, api_key_type="OpenAI"):
+    # Set OpenAI API endpoint URL
+    if api_key_type == "OpenAI":
+        endpoint_url = "https://api.openai.com/v1/engines"
+    elif api_key_type == "GitHub":
+        endpoint_url = "https://api.github.com"
+    else:
+        print(f"Invalid API key type: {api_key_type}")
+        return False
+
+    # Set GET request parameters
+    headers = {"Authorization": f"Bearer {api_key}"}
+
+    # Make GET request to API endpoint
+    try:
+        response = requests.get(endpoint_url, headers=headers)
+
+        # Check if request was successful
+        if response.status_code == 200:
+            return True
+        else:
+            print(f"{api_key_type} API key test failed with status code {response.status_code}: {response.text}")
+            return False
+    except requests.exceptions.RequestException as e:
+        print(f"{api_key_type} API key test failed with error: {e}")
+        return False
